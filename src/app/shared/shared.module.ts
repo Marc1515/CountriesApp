2bc0990e75196f56299fbb4284d6049005bedb68
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-06-28 07:04:57 +0200

    Declaramos y exportamos el componente Contact.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,26 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
