c359408dc00c3151cab6694710520fe315cb9f9a
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-03 12:04:51 +0200

    Para lograr la persistencia de datos en las dos secciones (byCapital y byCountry) implementamos el OnInit que guardará e iniciará las peticiones buscadas cuando se monte el componente.

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-country-page',
@@ -8,17 +8,27 @@ import { CountriesService } from '../../services/countries.service';
   styles: [
   ]
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit{
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
+  public initialValue: string = '';
 
   constructor (private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+  }
+
   searchByCountry(term: string): void {
+
+    this.isLoading = true;
 
     this.countriesService.searchCountry(term)
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       })
 
   }
