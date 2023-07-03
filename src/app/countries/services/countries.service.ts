c74eef2cad21c53a9e712c3e04b4d661c5919170
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-03 12:07:39 +0200

    En el servicio creamos y guardamos mediante un (tap) los países que buscaremos en la API.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, map, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 
 export class CountriesService {
 
-  private apiUrl: string = "https://restcountries.com/v3.1"
+  private apiUrl: string = "https://restcountries.com/v3.1";
+
+  public cacheStore: CacheStore = {
+    byCapital:    { term: '', countries: [] },
+    byCountries:  { term: '', countries: [] },
+    byRegion:     { region: '', countries: [] },
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -34,6 +42,9 @@ export class CountriesService {
     const url = `${this.apiUrl}/capital/${ term }`;
 
     return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byCapital = {term, countries} )
+      );
   }
 
   searchCountry( term: string ): Observable<Country[]> {
@@ -41,14 +52,20 @@ export class CountriesService {
     const url = `${this.apiUrl}/name/${ term }`;
 
     return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byCountries = {term, countries} )
+      );
 
   }
 
-  searchRegion( region: string ): Observable<Country[]> {
+  searchRegion( region: Region ): Observable<Country[]> {
 
     const url = `${this.apiUrl}/region/${ region }`;
 
     return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byRegion = {region, countries} )
+      );
 
   }
 
