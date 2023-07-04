0aff16c1886bd10601181058b0e29ae85e1171b0
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-04 12:18:49 +0200

    La intención de este código en los 3 documentos es que la sidebar no haga scroll vertical cuando se active. También se han eliminado los <li> contact y about por el motivo explicado en el anterior commit.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SharedSidebarService } from '../../services/shared-sidebar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [
-  ]
+  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
 
+  public isSidebarVisible: boolean = true;
+  private sidebarToggledSubscription!: Subscription;
+
+  constructor(private sharedSidebarService: SharedSidebarService) {}
+
+  ngOnInit() {
+    this.sidebarToggledSubscription = this.sharedSidebarService.sidebarToggled.subscribe((isOpen: boolean) => {
+      this.isSidebarVisible = !isOpen;
+      if(this.isSidebarVisible) {
+        document.body.style.overflow = 'auto';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if(this.sidebarToggledSubscription) {
+      this.sidebarToggledSubscription.unsubscribe();
+    }
+  }
 }
