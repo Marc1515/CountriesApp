aa89b5e6fbd448d6fee765eb6079b517e194ca89
Marc1515 <meq.1515@gmail.com>
2024-07-22 18:30:16 +0200

    Tilt library implemented

--- src/app/countries/components/country-cards/country-cards.component.ts
+++ src/app/countries/components/country-cards/country-cards.component.ts
@@ -1,16 +1,27 @@
-import { Component, HostListener, Input } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ElementRef,
+  ViewChildren,
+  QueryList,
+  HostListener,
+  Input,
+} from '@angular/core';
 import { Country } from '../../interfaces/country';
+import VanillaTilt from 'vanilla-tilt';
 
 @Component({
   selector: 'country-cards',
   templateUrl: './country-cards.component.html',
   styleUrls: ['./country-cards.component.css'],
 })
-export class CountryCardsComponent {
+export class CountryCardsComponent implements AfterViewInit {
   showButton = false;
 
   @Input()
   public countries: Country[] = [];
+
+  @ViewChildren('card') cards: QueryList<ElementRef> = new QueryList();
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -21,6 +32,17 @@ export class CountryCardsComponent {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
+    });
+  }
+
+  ngAfterViewInit() {
+    this.cards.forEach((card: ElementRef) => {
+      VanillaTilt.init(card.nativeElement, {
+        max: 25,
+        speed: 400,
+        glare: true,
+        'max-glare': 0.5,
+      });
     });
   }
 }
