07d21dcc6901db2da66c0707efcda55e9fa46289
Marc1515 <meq.1515@gmail.com>
2024-07-25 12:26:58 +0200

    This files are for the environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoibWFyYzE1IiwiYSI6ImNsejB6ZDAzbjJveHUya3NoMXlwNGVucHIifQ.qJod9IHmzFzLNhX8NLIizQ',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoibWFyYzE1IiwiYSI6ImNsejB6ZDAzbjJveHUya3NoMXlwNGVucHIifQ.qJod9IHmzFzLNhX8NLIizQ',
+  },
+};
