83a08eb86423ed650ef6ae3e921473f5c9a8329f
Marc1515 <meq.1515@gmail.com>
2024-07-25 10:10:21 +0200

    Now you can toggle the switch botton to change the countries view globaly

--- src/app/shared/services/shared-navbar.service.ts
+++ src/app/shared/services/shared-navbar.service.ts
@@ -5,11 +5,16 @@ import { Injectable, EventEmitter } from '@angular/core';
 })
 export class SharedNavbarService {
   public isNavbarVisibleService: boolean = true;
+  public isSwitchToggled: boolean = false;
 
   constructor() {}
 
   navbarToggled: EventEmitter<boolean> = new EventEmitter<boolean>();
-  sidebarToggled: EventEmitter<boolean> = new EventEmitter<boolean>();
   tagClicked: EventEmitter<string> = new EventEmitter<string>();
   switchToggled: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  toggleSwitch() {
+    this.isSwitchToggled = !this.isSwitchToggled;
+    this.switchToggled.emit(this.isSwitchToggled);
+  }
 }
