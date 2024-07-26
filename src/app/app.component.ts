1f80411a7ed47311b7302212de8bb616daa4f162
Marc1515 <meq.1515@gmail.com>
2024-07-26 12:30:46 +0200

    I had one horizontal hell scroll and I have decided to blow up all the fcking Taildwind, is a good idea for a small project but it's hard to keep it alive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'countryApp';
+  isScreenLarge: boolean;
+
+  constructor() {
+    this.isScreenLarge = window.innerWidth > 1024;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.isScreenLarge = window.innerWidth > 1024;
+  }
 }
